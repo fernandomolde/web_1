97da47c2d449d6944978d8d132038342c7af0f28
fernandomolde <ortizmoldef@gmail.com>
2022-04-01 10:36:42 +0200

    nuevo_codigo

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def server_static(filename):
 @jinja2_view('home.html') #Aqui le decimos como se llama el archivo
 def hola():
     cnx = sqlite3.connect(BASE_DATOS)
-    consulta = "select p.id ,p.nombre,p.apellidos,p.dni,to2.descripcion,p.id_numero from persona p left join T_ocupacion to2 on p.id_ocupacion = to2.id"
+    consulta = "select p.id ,p.nombre,p.apellidos,p.dni,to2.descripcion,tn.descripcion from persona p left join T_ocupacion to2 on p.id_ocupacion = to2.id LEFT join T_numero tn on tn.id=p.id_numero "
     cursor = cnx.execute(consulta)
     filas = cursor.fetchall() #Trae todas las filas para procesarlos
     cnx.close()
@@ -36,15 +36,28 @@ def mi_form(id=None):
     cursor = cnx.execute(consulta)
     numeros = cursor.fetchall()
 
+    #Vehiculos
+    consulta = "select * from T_vehiculo"
+    cursor = cnx.execute(consulta)
+    vehiculos = cursor.fetchall()
+
     if id is None: # Estamos en un alta
-        return {'ocupaciones':ocupaciones,'numeros':numeros}
+        return {'ocupaciones':ocupaciones,'numeros':numeros, 'vehiculos':vehiculos}
     else:
-        consulta = "select id,nombre,apellidos,dni from persona where id = ?"
+        consulta = "select id,nombre,apellidos,dni,id_ocupacion,id_numero from persona where id = ?"
         cursor = cnx.execute(consulta,(id,)) #La coma despues del ID,dice que es una tupla
         filas = cursor.fetchone() # Obtiene 1 fila para procesarla
 
+    #   Mis_vehiculos
+    consulta = "select id_vehiculo from persona_vh where id_persona = {id}"
+    cursor = cnx.execute(consulta)
+    tmp = cursor.fetchall()
+    mis_vehiculos = []
+    for t in tmp:
+        mis_vehiculos.append(t[0])
+
     cnx.close()
-    return {'datos':filas,'ocupaciones':ocupaciones,'numeros': numeros}
+    return {'datos':filas,'ocupaciones':ocupaciones,'numeros': numeros,'vehiculos':vehiculos,'mis_vehiculos':mis_vehiculos}
 
 @route('/guardar', method='POST')
 def guardar(): 
@@ -54,14 +67,19 @@ def guardar():
     id = request.POST.id
     ocupacion = request.POST.ocupacion
     numero = request.POST.numero
+    vehiculos = request.POST.dict['vehiculo'] # Esto es una lista vehiculos
     cnx = sqlite3.connect(BASE_DATOS)
 
 
     if id =='': #Alta
 
         consulta = "insert into persona(nombre, apellidos,dni,id_ocupacion,id_numero) values (?,?,?,?,?)"
-        cnx.execute(consulta,(nombre,apellidos,dni,ocupacion,numero))
-
+        tmp = cnx.execute(consulta,(nombre,apellidos,dni,ocupacion,numero))
+        nuevo_id = tmp.lastrowid # Para conseguir el ultimo id de la fila
+        # ----------------------
+        for v in vehiculos:
+            nuevos_vh = f'insert into persona_vh(id_persona,id_vehiculo) values ({nuevo_id},{v})' # insertamos en la tabla personas_vh, 
+            cnx.execute(nuevos_vh)                                                                # el id de persona y de vehiculo y de valor,nuevo id y v que lo hemos definido antes
     else: #Actualizacion
          
         consulta = "update persona set nombre = ?, apellidos = ?, dni =?, id_ocupacion=?, id_numero=? where id =?"
@@ -81,7 +99,7 @@ def borrar(id=None):
         # consulta = f'delete from persona where id ="{id}"'
         cnx.execute(consulta)
         cnx.commit()
-        cnx.close()                     # Codigo para borrar
+        cnx.close()       # Codigo para borrar
     redirect('/')
 
 run(host='localhost',port=8080,debug=True)
